afd70d3af432a01c513d3571b0e98860697f0cbc
Jeremy Herve <jeremy@jeremy.hu>
2018-10-02 11:54:07 -0400

    WIP: first block attributes and a bit of the frontend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { createElement } = wp.element;
 const { registerBlockType } = wp.blocks;
+const { RichText, InspectorControls } = wp.editor;
+const { RangeControl } = wp.components;
 
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 
 registerBlockType( 'gm18-recipe-block/recipe-block', {
 	title: __( 'Recipe' ),
@@ -9,14 +11,68 @@ registerBlockType( 'gm18-recipe-block/recipe-block', {
 	icon: 'carrot',
 	category: 'common',
 	keywords: [ __( 'recipes' ), __( 'food' ), __( 'jetpack' ) ],
+	attributes: {
+		title: {
+			type: 'string',
+		},
+		servings: {
+			type: 'int',
+		},
+		time: {
+			type: 'string',
+		},
+		difficulty: {
+			type: 'string',
+		},
+		print: {
+			type: 'string',
+		},
+		source: {
+			type: 'string',
+		},
+		sourceurl: {
+			type: 'string',
+		},
+		image: {
+			type: 'string',
+		},
+		description: {
+			type: 'string',
+		},
+	},
 
 	// In the admin.
-	edit(props) {
-		return <p>Hello Editor</p>;
+	edit( props ) {
+		function updateServingsAttribute( newValue ) {
+			props.setAttributes({
+				servings: newValue
+			});
+		}
+
+		return <p className='recipe-options'>
+			<InspectorControls>
+				<RangeControl
+					label={ __( 'Servings' ) }
+					value={ props.attributes.servings }
+					initialPosition={ 2 }
+					onChange={ updateServingsAttribute }
+					min={ 1 }
+					max={ 20 }
+				/>
+			</InspectorControls>
+			<textarea>
+				{props.attributes.title}
+			</textarea>
+		</p>;
 	},
 
 	// After saving, so this is what appears on the frontend.
-	save(props) {
-		return <p>Hello Frontend</p>;
+	save( props ) {
+		return <div class="hrecipe jetpack-recipe" itemscope itemtype="https://schema.org/Recipe">
+			<h3 class="jetpack-recipe-title" itemprop="name">{ props.attributes.title }</h3>
+			<ul class="jetpack-recipe-meta">
+				<li class="jetpack-recipe-servings" itemprop="recipeYield"><strong>{ __( 'Servings' ) }: </strong>{ props.attributes.servings }</li>',
+			</ul>
+		</div>;
 	}
 });
